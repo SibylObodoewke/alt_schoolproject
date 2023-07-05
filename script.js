c399935fa77bab4382f56e6be5173dc4cd25351d
sibyl obodoekwe <sibylnkaechi@gmail.com>
2023-07-05 22:03:05 +0100

    Update and rename index.js to script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+const toggles = Array.from(document.querySelectorAll(".toggle"));
+const contentDivs = Array.from(document.querySelectorAll(".content"));
+const icons = Array.from(document.querySelectorAll(".toggle img"));
+
+function toggleContent(index) {
+  const currentContentDiv = contentDivs[index];
+  const currentIcon = icons[index];
+  const isOpen = parseInt(currentContentDiv.style.height) !== currentContentDiv.scrollHeight;
+
+  contentDivs.forEach((contentDiv, i) => {
+    if (i === index) {
+      currentContentDiv.style.height = isOpen ? "0px" : currentContentDiv.scrollHeight + "px";
+      toggles[i].style.color = isOpen ? "#151330" : "#0084e9";
+      currentIcon.src = isOpen ? "/images/plus.svg" : "/images/minus.svg";
+    } else {
+      contentDiv.style.height = "0px";
+      toggles[i].style.color = "#111130";
+      icons[i].src = "/images/plus.svg";
+    }
+  });
+}
+
+toggles.forEach((toggle, i) => {
+  toggle.addEventListener("click", () => {
+    toggleContent(i);
+  });
+});
